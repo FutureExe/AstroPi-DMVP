f238a82ef80ddce2215bfdd506378308f8b9a39b
FutureExe <vitus.pitt@protonmail.com>
2026-01-21 10:30:42 +0000

    add csv test and expanded module import

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from pathlib import Path
+from datetime import datetime, timedelta 
+from time import sleep
+import csv
+
+base_folder = Path(__file__).parent.resolve()
+data_file = base_folder / "data.csv"
+for i in range(10):
+    with open(data_file, "w", buffering=1) as f:
+        writer = csv.writer(f)
+        writer.writerow("test")
